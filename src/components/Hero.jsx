e9631ceea21ae970eb25a0090b8aa3ec8c9c230a
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-07-07 09:11:54 +0100

    fix: hot fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
   const navigate = useNavigate();
 
   const handleBtnClick = () => {
-    navigate("/galleries");
+    navigate("/galleries/products");
   };
 
   useEffect(() => {
