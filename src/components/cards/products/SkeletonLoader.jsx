d4b1cdba638436c208fa3ffbfd0ba5922686313d
oree <oreoluwaajayyiruth@gmail.com>
2024-07-13 15:14:10 +0100

    feat: api for all products added

--- src/components/cards/products/SkeletonLoader.jsx
+++ src/components/cards/products/SkeletonLoader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SkeletonLoader = () => {
   return (
     <div className="animate-pulse flex flex-col gap-4 p-4 border border-gray-200 rounded-lg">
-      <div className="h-48 bg-gray-300 rounded-lg"></div>
+      <div className="h-[490px] bg-gray-300 rounded-lg"></div>
       <div className="h-4 bg-gray-300 rounded w-3/4"></div>
       <div className="h-4 bg-gray-300 rounded w-1/2"></div>
       <div className="h-4 bg-gray-300 rounded w-5/6"></div>
