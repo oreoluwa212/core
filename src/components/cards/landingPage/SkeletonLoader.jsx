3d5201432c168d24f44dcc64d72b33adb697f5ab
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2024-07-14 08:09:46 +0100

    feat: fetched the products on Landing from Timbu

--- src/components/cards/landingPage/SkeletonLoader.jsx
+++ src/components/cards/landingPage/SkeletonLoader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SkeletonLoader = () => {
   return (
-    <div className="w-[300px] h-fit bg-transparent font-montserrat text-white border-2 border-gray-600 rounded-lg shadow-lg relative p-3">
+    <div className="w-[180px] h-fit bg-transparent font-montserrat text-white border-2 border-gray-600 rounded-lg shadow-lg relative p-3">
       <div className="h-[240px] bg-gray-700 rounded-t-lg animate-pulse mb-4"></div>
       <div className="space-y-3">
         <div className="h-6 bg-gray-700 rounded animate-pulse w-3/4 mx-auto"></div>
